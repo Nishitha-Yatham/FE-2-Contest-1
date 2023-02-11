5637e8ff09a9eda8a0393dc640cfe0de14242ced
Nishitha-Yatham <nishithay1054@gmail.com>
2023-02-11 13:14:06 +0530

    initial commit

--- script.js
+++ script.js
@@ -8,20 +8,43 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(arr => {
+    if (arr.marks > 50) {
+      console.log(arr);
+    }
+  });
 }
 
+
 function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
+  arr.forEach(arr =>{
+    if (arr.marks > 50) {
+      console.log(arr);
+    }
+  })
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newobj={
+    id:4,name:"susan",age:"20",marks:45
+  }
+    arr.push(newobj);
+    console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr = arr.filter(arr => arr.marks >= 50);
+  console.log(arr);
 }
 
 function concatenateArray() {
-  //Write your code here, just console.log
+  let newArray=[
+    {id: 5, name: "Sandya", age: "29", marks: 62},
+    {id: 6, name: "Nishi", age: "22", marks: 70},
+    {id: 7, name: "Sri", age: "23", marks: 81}
+  ]
+  let ConcatArray = arr.concat(newArray);
+  console.log(ConcatArray);
 }
